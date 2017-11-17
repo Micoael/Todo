32365ac11d68c368089591b9c782497919c2ff63
Primo_214 <micoael@qq.com>
2017-11-17 10:27:05 +0800

    Create package-info.java
    Notes is the first step a.a

---
+++ src/wanna/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * This project offers you some basically
+ * things for this project
+ * We'r storing your reminders in a databank,k
+ * and we can search them according your law.
+ * <b>Here are some of rhe methods...</b>
+ * -Add a new plan
+ * -Delete a plan
+ * -Set a timer about the plan
+ * -Set a <b>daily</b> routine there.
+ * <h1> More infos. are goin to join later.</h1>
+/**
+ * @author Zhang guangwei
+ */
+package wanna;
