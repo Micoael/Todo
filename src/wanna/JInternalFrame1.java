d5cf215feac591e9cba4cf2eae53ca3783ee73cd
zgw <micoael@qq.com>
2017-11-25 12:41:15 +0800

    Update JInternalFrame1.java

--- src/wanna/JInternalFrame1.java
+++ src/wanna/JInternalFrame1.java
@@ -1,9 +1,13 @@
+package wanna;
 import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
+
 import java.awt.event.*;
 import java.awt.*;
 
 public class JInternalFrame1 extends JFrame implements ActionListener {
-
+	JTextField Tplan;
+	JTextField Tdesc;
 	JDesktopPane desktopPane;
 	int count = 1;
 	//a constructor.
@@ -18,6 +22,16 @@ public class JInternalFrame1 extends JFrame implements ActionListener {
 		
 		desktopPane = new JDesktopPane();
 		contentPane.add(desktopPane);
+		GroupLayout gl_desktopPane = new GroupLayout(desktopPane);
+		gl_desktopPane.setHorizontalGroup(
+			gl_desktopPane.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 332, Short.MAX_VALUE)
+		);
+		gl_desktopPane.setVerticalGroup(
+			gl_desktopPane.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 275, Short.MAX_VALUE)
+		);
+		desktopPane.setLayout(gl_desktopPane);
 
 		setSize(350, 350);
 		show();
@@ -34,30 +48,8 @@ public class JInternalFrame1 extends JFrame implements ActionListener {
 	 * 产生一个可关闭、可改变大小、具有标题、可最大化与最小化的Internal Frame.
 	 */
 	public void actionPerformed(ActionEvent e) {
-		JInternalFrame internalFrame = new JInternalFrame("Internal Frame "
-				+ (count++), true, true, true, true);
-
-		internalFrame.setLocation(20, 20);
-		internalFrame.setSize(200, 200);
-		internalFrame.setVisible(true);
 		
-		Container icontentPane = internalFrame.getContentPane();
-		JTextArea textArea = new JTextArea();
-		JButton b = new JButton("Internal Frame Button");
-		/*
-		 * 将JTextArea与JButton对象加入JInternalFrame中。由此呆知，JInteranlFrame加入组件
-		 * 的方式与JFrame是一模一样。
-		 */
-		icontentPane.add(textArea, "Center");
-		icontentPane.add(b, "South");
-	
-		desktopPane.add(internalFrame);
-
-		try {
-			internalFrame.setSelected(true);
-		} catch (java.beans.PropertyVetoException ex) {
-			System.out.println("Exception while selecting");
-		}
+		addNewList("hi");
 	}
 	/**
 	* This method is a add method and it could add a new frame in the JFrame.
@@ -65,24 +57,71 @@ public class JInternalFrame1 extends JFrame implements ActionListener {
 	*
 	**/
 	public void addNewList(String titlename){
-		JInternalFrame internalFrame = new JInternalFrame(titlename, true, true, true, true);
-
-		internalFrame.setLocation(20, 20);
-		//TODO: Let the interalFrame *Seprate*
-		internalFrame.setSize(200, 200);
-		internalFrame.setVisible(true);
 		
-		Container icontentPane = internalFrame.getContentPane();
-		JTextArea textArea = new JTextArea();
-		JButton b = new JButton("Internal Frame Button");
-		/*
-		 * 将JTextArea与JButton对象加入JInternalFrame中。由此呆知，JInteranlFrame加入组件
-		 * 的方式与JFrame是一模一样。
-		 */
-		icontentPane.add(textArea, "Center");
-		icontentPane.add(b, "South");
-	
-		desktopPane.add(internalFrame);
+		JInternalFrame inframe = new JInternalFrame(titlename, true, true, true, true);
+
+		inframe.setLocation(20, 20);
+		
+		inframe.setSize(200, 200);
+		inframe.setVisible(true);
+		Tplan=new JTextField();
+		Tdesc=new JTextField();
+		JLabel lbPlan=new JLabel("Plan");
+		JLabel lbDesc=new JLabel("Descs");
+		
+		
+		
+		
+		Tplan.setBounds(48, 12, 188, 30);
+		lbPlan.setBounds(0, 0, 65, 50);
+		lbPlan.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lbDesc.setBounds(0, 43, 50, 50);
+		lbDesc.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		Tdesc.setBounds(48, 52, 188, 37);
+		JButton btnE = new JButton("\u221A / e");
+		btnE.setBounds(84, 102, 88, 27);
+		
+		
+		
+		
+		
+		
+		
+		
+		btnE.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(Tdesc.isEnabled()&&Tplan.isEnabled()){
+					Tdesc.setEnabled(false);
+					Tplan.setEnabled(false);
+					inframe.setTitle(Tplan.getText()+" - "+Tdesc.getText().substring(0,2)+" ...");
+				}else{
+					Tdesc.setEnabled(true);
+					Tplan.setEnabled(true);
+					inframe.setTitle(Tplan.getText()+" - "+Tdesc.getText().substring(0,2)+" ...");
+				}
+				
+				
+			}
+		});
+		
+		Tplan.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e) {
+				inframe.setTitle(Tplan.getText());
+			}
+		});
+		
+		
+		
+		
+		inframe.getContentPane().add(lbPlan);
+		inframe.getContentPane().add(lbDesc);
+		inframe.getContentPane().add(Tplan);
+		inframe.getContentPane().add(Tdesc);
+		inframe.getContentPane().setLayout(null);
+		inframe.getContentPane().add(btnE);
+		
+		desktopPane.add(inframe);
 
 	}
 	
